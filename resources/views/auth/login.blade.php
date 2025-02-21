3a74c00b0f96447ed8de34d38c6206e9e4ea3af3
santiagoconda <yonatanpazu2@gmail.com>
2025-02-21 16:53:29 -0500

    Login app

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+    <form action="">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="password" placeholder="Password">
+        <a href="">Has olvidado tu contraseña?</a>
+        <button type="submit">Inicaiar sesión</button>
+    </form> 
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\authController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', [authController::class, 'login'])->name('login');
